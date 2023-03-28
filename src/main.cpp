f9158f46387f8c6ab17301f31c53e33497228eb3
Rémi K <remi.kiragossian@gmail.com>
2023-03-28 09:08:54 +0200

    ADC en double

--- src/main.cpp
+++ src/main.cpp
@@ -160,7 +160,7 @@ void setup()
 /// test ACD 
 
     adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_11);
+    // adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
     adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_11);
     adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
     adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_11);
@@ -199,9 +199,10 @@ void setup()
   dallas.detect = dallaspresent();
   #endif
 
-  // Setup the ADC
-  adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
-  adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_11);
+  // // Setup the ADC
+  // adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_11);
+  // adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
+  // adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_11);
 
   //analogReadResolution(ADC_BITS);
   pinMode(ADC_INPUT, INPUT);
